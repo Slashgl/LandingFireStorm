a4a99f0f83e765b89721594e78800cd76722c6bd
daysg <days.grace2015@yandex.ru>
2023-07-16 14:11:57 +0300

    feat: add card flip when scrolling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './index.html';
 import './index.scss';
 
+document.addEventListener("DOMContentLoaded", () => {
+    const cardsFront = document.querySelectorAll('.cards-front');
+    const cardsBack = document.querySelectorAll('.cards-back');
+
+    window.addEventListener('scroll',  () => {
+        const height = document.documentElement.scrollTop;
+        if(height > 300) {
+            cardsFront.forEach(cards => {
+                cards.style.transform = 'rotateY(180deg)'
+            })
+
+            cardsBack.forEach(cards => {
+                cards.style.transform = 'rotateY(0deg)'
+            })
+        }else {
+            console.log('false')
+        }
+    })
+
+});
